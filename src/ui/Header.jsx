76f6b3333861616123e6ce8aa6023252d1d8d1ef
TTSC <wsctt91@gmail.com>
2025-01-01 23:58:15 +0900

    Resposive Design in Tailwindcss

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast Pizza Co.</Link>
+    <header className="border-b-1 border-stone-500 bg-yellow-500 px-4 py-4 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>CharlesWu</p>
+      <Username />
     </header>
   );
 }
